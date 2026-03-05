a8624f218c343e25b7bc15766232b2ce1124cc7d
Carsten Milling <carsten.milling@gmail.com>
2026-03-05 14:18:50 +0100

    Add NavBar
    upgrading @dracor/react for tanstack router fix

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,31 +1,49 @@
-import {createRootRoute, Link, Outlet} from '@tanstack/react-router';
+import {createRootRoute, Outlet} from '@tanstack/react-router';
 import {TanStackRouterDevtools} from '@tanstack/react-router-devtools';
+import {NavBar} from '@dracor/react';
+import pkg from '../../package.json';
 import {fetchApiInfo, fetchCorpora} from '../loaders';
 
 const RootLayout = () => {
   const {info, corpora} = Route.useLoaderData();
   return (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link
-          to="/doc/$"
-          params={{_splat: 'about'}}
-          className="[&.active]:font-bold"
-        >
-          About
-        </Link>{' '}
-        <Link to="/doc/api" className="[&.active]:font-bold">
-          API
-        </Link>{' '}
+      <NavBar
+        title="ELTeC"
+        version={`version ${pkg.version}`}
+        gitHubUrl="https://github.com/clscor-io/eltec-api"
+        navItems={[
+          {
+            label: 'About',
+            items: [
+              {label: 'What is ELTeC', to: '/doc/$', params: {_splat: 'about'}},
+              {
+                label: 'Imprint and GDPR',
+                to: '/doc/$',
+                params: {_splat: 'imprint-and-gdpr'},
+              },
+            ],
+          },
+          {
+            label: 'Corpora',
+            items: corpora.map(({title, name}) => ({
+              label: title,
+              to: '/corpora/$corpusId',
+              params: {corpusId: name},
+            })),
+          },
+          {
+            label: 'API',
+            to: '/doc/api',
+          },
+        ]}
+      />
+      <Outlet />
+      <div>
         <small>
-          ELTeC API {info.version}, {corpora.length} corpora
+          ELTeC API: {info.version}, eXist: {info.existdb}
         </small>
       </div>
-      <hr />
-      <Outlet />
       <TanStackRouterDevtools />
     </>
   );
